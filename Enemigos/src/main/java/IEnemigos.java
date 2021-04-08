f65d586fff383b0e44ef9381e3285c2c0ef098d4
Sof-valur <62963590+Sof-valur@users.noreply.github.com>
2021-04-07 22:22:59 -0600

    Clase Enemigos
    Se crea la clase enemigos y lo básico que deben de tener, faltan las hileras

---
+++ Enemigos/src/main/java/IEnemigos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 50684
+ */
+public interface IEnemigos {
+    public double hitPoints();
+    public int velocidad();
+    public double resistencia();
+    public double bajarVida();
+}
